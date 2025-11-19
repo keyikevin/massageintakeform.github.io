161f1f4b31e12ff37af803ed297855bd31a80a96
Kevin Liu <kevin.liu@keyitech.net>
2025-11-19 00:45:24 -0600

    Fix customer redirect

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3650673442"
+    serviceWorkerVersion: "3665164948"
   }
 });
